8d80c6e9c98006ec0fc16b2a4375273ce81809c0
Travis Reeder <treeder@gmail.com>
2014-02-06 11:38:58 -0800

    Yikes, that was bad. Had reference to iron_cache_rails in here.

--- lib/iron_cache.rb
+++ lib/iron_cache.rb
@@ -3,8 +3,3 @@ require "iron_cache/version"
 require 'iron_cache/caches'
 require 'iron_cache/items'
 require 'iron_cache/client'
-
-# session store
-if defined? ActionDispatch
-  require 'action_dispatch/session/iron_cache'
-end
